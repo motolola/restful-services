e61c1f130e6e11ce065b4c0759b325c228ee7bb2
motolola <motolola@icloud.com>
2018-01-28 18:16:16 +0000

    Updates

--- src/main/java/com/motolola/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/motolola/restfulwebservices/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.motolola.restfulwebservices.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.motolola.restfulwebservices.bean.HelloWorldBean;
@@ -17,6 +18,10 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World!");
+	}
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World! %s", name));
 	}
 
 }
