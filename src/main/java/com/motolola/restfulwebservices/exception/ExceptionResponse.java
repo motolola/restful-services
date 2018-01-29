34d7d7e1d2234f015a856d5b58dac5c939ba995a
motolola <motolola@icloud.com>
2018-01-29 19:42:50 +0000

    Customised and generalised Exception Handling

---
+++ src/main/java/com/motolola/restfulwebservices/exception/ExceptionResponse.java
@@ -0,0 +1,33 @@
+package com.motolola.restfulwebservices.exception;
+
+import java.util.Date;
+
+public class ExceptionResponse {
+	
+	private Date timestamps;
+	private String message;
+	private String details;
+	
+	public ExceptionResponse(Date timestamps, String message, String details) {
+		super();
+		this.timestamps = timestamps;
+		this.message = message;
+		this.details = details;
+	}
+
+	public Date getTimestamps() {
+		return timestamps;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+	
+	
+	
+
+}
